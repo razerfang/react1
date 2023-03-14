d4c9d95d63a47085f5b237758762c6bd70ad3c64
prajwal <prajwalj.20.decp@acharya.ac.in>
2023-03-14 19:29:11 +0530

    didnt do much. not feeling into it rn

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,21 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
+
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2023");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
